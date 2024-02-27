98f672ac2eb62a9fd685a951f37722b3ee2ff706
DaniellaRowandja <133262490+DaniellaRowandja@users.noreply.github.com>
2024-02-27 15:43:10 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import pygwalker as pyg
+
+""" df = pd.read_csv("/Users/rowandjadaniella/Downloads/apple_quality.csv")
+gwalker = pyg.walk(df) 
+ """
+
+st._show("lalaland")
